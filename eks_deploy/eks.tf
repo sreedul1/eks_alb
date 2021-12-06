0865e8b797c1b9d29812423cfb09d4894d2b2706
sreedul1 <81852397+sreedul1@users.noreply.github.com>
2021-12-06 12:06:52 +0530

    instance type

--- eks_deploy/eks.tf
+++ eks_deploy/eks.tf
@@ -7,7 +7,7 @@ module "eks_deployment" {
   cluster_version = "1.21"
   eks_vpc_id      = "vpc-00c578283e0f21a73"
   eks_subnet_ids  = ["subnet-05d165b54c1676f44","subnet-01dc25fcf3c3eb09c","subnet-05b48afa9834cab61"]
-  instance_types  = ["t3.micro"]
+  instance_types  = ["t2.micro"]
   ssh_key         = "test_eks"
   tags            = { Name = "Kong_Dev_eks_cluster" }
   desired_size    = 3
